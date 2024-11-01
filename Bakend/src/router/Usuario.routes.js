0a2d9e8542b7d2facf6a88bd2f19f7ae165c34e1
Jertroll <jeremyblanco20@gmail.com>
2024-11-01 03:41:12 -0600

    cambios

--- Bakend/src/router/Usuario.routes.js
+++ Bakend/src/router/Usuario.routes.js
@@ -5,12 +5,13 @@ import { authenticateToken, authorizeRole } from '../Middleware/JwtAuth.js';
 const router = Router();
 
 // Definir rutas y asociarlas a los métodos del controlador
-router.get('/usuarios', authenticateToken, authorizeRole(['Administrador']), UsuarioController.getAllUsers);
-//router.get('/usuarios',UsuarioController.getAllUsers);
+//router.get('/usuarios', authenticateToken, authorizeRole(['Administrador']), UsuarioController.getAllUsers);
+router.get('/usuarios',UsuarioController.getAllUsers);
 router.post('/usuarios', UsuarioController.createUser);
 router.get('/usuarios/:cedula', authenticateToken, authorizeRole(['Administrador']), UsuarioController.getUserById);
 router.put('/usuarios/:cedula', UsuarioController.updateUser);
-router.delete('/usuarios/:cedula', authenticateToken, authorizeRole(['Administrador']), UsuarioController.deleteUser);
+//router.delete('/usuarios/:cedula', authenticateToken, authorizeRole(['Administrador']), UsuarioController.deleteUser);
+router.delete('/usuarios/:cedula', UsuarioController.deleteUser);
 router.post('/login', UsuarioController.login);
 
 
