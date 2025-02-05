e32843ebecb94d5ed02453b639cee5041a147470
Kimyu15 <kimberley.gomez.quesada@est.una.ac.cr>
2025-02-05 17:27:03 -0600

    .

--- Bakend/src/router/Producto.routes.js
+++ Bakend/src/router/Producto.routes.js
@@ -4,7 +4,7 @@ import ProductoController from '../Controller/ProductoController.js';
 const router = Router();
 
 router.get('/productos', ProductoController.getAll);
-router.get('/productos/:id', ProductoController.getById);
+router.post('/productos/buscar', ProductoController.busqueda);
 router.post('/productos', ProductoController.create);
 router.put('/productos/:id', ProductoController.update);
 router.delete('/productos/:id', ProductoController.delete);
