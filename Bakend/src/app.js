c60b7c4e9e5713741800922c0b1c971df7236f9d
Kristhel04 <149755958+Kristhel04@users.noreply.github.com>
2025-02-18 20:33:16 -0600

    Carrito.Prueba

--- Bakend/src/app.js
+++ Bakend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import usuariosR from "./router/Usuario.routes.js"
 import productosR from "./router/Producto.routes.js"
 import categoriasR from './router/Categoria.routes.js';
+import carritosR from "./router/Carrito.routes.js";
 import 'dotenv/config';
 import cors from 'cors';
 
@@ -12,4 +13,5 @@ app.use(express.json());
 app.use('/api/v1', usuariosR);
 app.use('/api/v1', productosR);
 app.use('/api/v1', categoriasR);
+app.use('/api/v1', carritosR);
 export default app
