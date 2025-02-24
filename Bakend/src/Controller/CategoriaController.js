72ff65be48bac1d809882cf89afa8d03e8d72f74
Jertroll <jeremyblanco20@gmail.com>
2025-02-24 15:44:41 -0600

    Cambios Jer
    En estos cambios se trato de implementar que la funcion devuelva la imagen correctamente al front-end

--- Bakend/src/Controller/CategoriaController.js
+++ Bakend/src/Controller/CategoriaController.js
@@ -8,147 +8,151 @@ const uploadDir = path.join("public", "ImgCategorias");
 
 // Verificar si la carpeta existe, si no, crearla
 if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
+  fs.mkdirSync(uploadDir, { recursive: true });
 }
 
 // Configuración de multer
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadDir);
-    },
-    filename: (req, file, cb) => {
-        const ext = path.extname(file.originalname).toLowerCase();
-        const filename = `${Date.now()}${ext}`;
-        cb(null, filename);
-    }
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname).toLowerCase();
+    const filename = `${Date.now()}${ext}`;
+    cb(null, filename);
+  },
 });
 
-//Fintro para que solo se acepten imagenes 
+//Fintro para que solo se acepten imagenes
 const fileFilter = (req, file, cb) => {
-    const allowedExtensions = [".jpg", ".jpeg", ".png"];
-    const ext = path.extname(file.originalname).toLowerCase();
-    if (!allowedExtensions.includes(ext)) {
-        return cb(new Error("Formato de archivo no permitido"), false);
-    }
-    cb(null, true);
+  const allowedExtensions = [".jpg", ".jpeg", ".png"];
+  const ext = path.extname(file.originalname).toLowerCase();
+  if (!allowedExtensions.includes(ext)) {
+    return cb(new Error("Formato de archivo no permitido"), false);
+  }
+  cb(null, true);
 };
 
 //Subida de la imagen al storage
 const upload = multer({ storage, fileFilter }).single("imagen");
 
-
 const CategoriaController = {
-    
-    async getAll(req, res) {
-        try {
-            const categorias = await Categoria.findAll();
-            res.status(200).json(categorias);
-        } catch (error) {
-            res.status(500).json({ message: "Error al obtener las categorías", error });
-        }
-    },
-
-
-    async getById(req, res) {
-        try {
-            const { id } = req.params;
-            const categoria = await Categoria.findByPk(id);
-            if (categoria) {
-                res.status(200).json(categoria);
-            } else {
-                res.status(404).json({ message: "Categoría no encontrada" });
-            }
-        } catch (error) {
-            res.status(500).json({ message: "Error al obtener la categoría", error });
-        }
-    },
-
-    // Crear una nueva categoría 
-    async create(req, res) {
-        upload(req, res, async (err) => {
-            if (err) {
-                return res.status(400).json({ message: err.message });
-            }
-            try {
-                const { nombre_categoria, descripcion_categoria } = req.body;
-                const imagen = req.file ? req.file.filename : null;
-                const nuevaCategoria = await Categoria.create({
-                    nombre_categoria,
-                    descripcion_categoria,
-                    imagen
-                });
-                res.status(201).json(nuevaCategoria);
-            } catch (error) {
-                res.status(500).json({ message: "Error al crear la categoría", error });
-            }
-        });
-    },
-
-    // Actualizar una categoría 
-    async update(req, res) {
-        upload(req, res, async (err) => {
-            if (err) {
-                return res.status(400).json({ message: err.message });
-            }
-            try {
-                const { id } = req.params;
-                const { nombre_categoria, descripcion_categoria } = req.body;
-                const categoria = await Categoria.findByPk(id);
-
-                if (!categoria) {
-                    return res.status(404).json({ message: "Categoría no encontrada" });
-                }
-
-                let nuevaImagen = categoria.imagen;
-                if (req.file) {
-                    // Eliminar la imagen anterior si existe
-                    if (categoria.imagen) {
-                        const oldImagePath = path.join(uploadDir, categoria.imagen);
-                        if (fs.existsSync(oldImagePath)) {
-                            fs.unlinkSync(oldImagePath);
-                        }
-                    }
-                    nuevaImagen = req.file.filename;
-                }
-
-                await categoria.update({ 
-                    nombre_categoria, 
-                    descripcion_categoria, 
-                    imagen: nuevaImagen 
-                });
-
-                res.status(200).json(categoria);
-            } catch (error) {
-                res.status(500).json({ message: "Error al actualizar la categoría", error });
-            }
-        });
-    },
-
-    // Eliminar una categoría
-    async delete(req, res) {
-        try {
-            const { id } = req.params;
-            const categoria = await Categoria.findByPk(id);
-
-            if (!categoria) {
-                return res.status(404).json({ message: "Categoría no encontrada" });
-            }
-
-            // Eliminar la imagen si existe
-            if (categoria.imagen) {
-                const imagePath = path.join(uploadDir, categoria.imagen);
-                if (fs.existsSync(imagePath)) {
-                    fs.unlinkSync(imagePath);
-                }
-            }
-
-            await categoria.destroy();
-            res.status(200).json({ message: "Categoría eliminada correctamente" });
-
-        } catch (error) {
-            res.status(500).json({ message: "Error al eliminar la categoría", error });
-        }
+  async getAll(req, res) {
+    try {
+      const categorias = await Categoria.findAll();
+      res.status(200).json(categorias);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Error al obtener las categorías", error });
     }
+  },
+
+  async getById(req, res) {
+    try {
+      const { id } = req.params;
+      const categoria = await Categoria.findByPk(id);
+      if (categoria) {
+        res.status(200).json(categoria);
+      } else {
+        res.status(404).json({ message: "Categoría no encontrada" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Error al obtener la categoría", error });
+    }
+  },
+
+  // Crear una nueva categoría
+  async create(req, res) {
+    upload(req, res, async (err) => {
+      if (err) {
+        return res.status(400).json({ message: err.message });
+      }
+      try {
+        const { nombre_categoria, descripcion_categoria } = req.body;
+        const imagen = req.file ? `/public/ImgCategorias/${req.file.filename}` : '/public/ImgCategorias/No imagen.jpg';
+
+
+        const nuevaCategoria = await Categoria.create({
+          nombre_categoria,
+          descripcion_categoria,
+          imagen,
+        });
+        res.status(201).json(nuevaCategoria);
+      } catch (error) {
+        res.status(500).json({ message: "Error al crear la categoría", error });
+      }
+    });
+  },
+
+  // Actualizar una categoría
+  async update(req, res) {
+    upload(req, res, async (err) => {
+      if (err) {
+        return res.status(400).json({ message: err.message });
+      }
+      try {
+        const { id } = req.params;
+        const { nombre_categoria, descripcion_categoria } = req.body;
+        const categoria = await Categoria.findByPk(id);
+
+        if (!categoria) {
+          return res.status(404).json({ message: "Categoría no encontrada" });
+        }
+
+        let nuevaImagen = categoria.imagen;
+        if (req.file) {
+          // Eliminar la imagen anterior si existe
+          if (categoria.imagen) {
+            const oldImagePath = path.join(uploadDir, categoria.imagen);
+            if (fs.existsSync(oldImagePath)) {
+              fs.unlinkSync(oldImagePath);
+            }
+          }
+          nuevaImagen = req.file.filename;
+        }
+
+        await categoria.update({
+          nombre_categoria,
+          descripcion_categoria,
+          imagen: nuevaImagen,
+        });
+
+        res.status(200).json(categoria);
+      } catch (error) {
+        res
+          .status(500)
+          .json({ message: "Error al actualizar la categoría", error });
+      }
+    });
+  },
+
+  // Eliminar una categoría
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      const categoria = await Categoria.findByPk(id);
+
+      if (!categoria) {
+        return res.status(404).json({ message: "Categoría no encontrada" });
+      }
+
+      // Eliminar la imagen si existe
+      if (categoria.imagen) {
+        const imagePath = path.join(uploadDir, categoria.imagen);
+        if (fs.existsSync(imagePath)) {
+          fs.unlinkSync(imagePath);
+        }
+      }
+
+      await categoria.destroy();
+      res.status(200).json({ message: "Categoría eliminada correctamente" });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Error al eliminar la categoría", error });
+    }
+  },
 };
 
 export default CategoriaController;
--- Bakend/src/index.js
+++ Bakend/src/index.js
@@ -1,9 +1,17 @@
 import app from './app.js'
 
+import express from 'express';
+import path from 'path';
+import fs from 'fs';
+
+// Esto es equivalente a __dirname en módulos ES
+const __dirname = path.resolve();
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 //Rutas
 app.get('/', (req, res) => {
