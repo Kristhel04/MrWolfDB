425c09ea527ea826c2bddb1f3c7551e34443e67a
Jertroll <jeremyblanco20@gmail.com>
2025-02-25 23:15:12 -0600

    Avance

--- Bakend/src/app.js
+++ Bakend/src/app.js
@@ -5,11 +5,12 @@ import categoriasR from './router/Categoria.routes.js';
 import carritosR from "./router/Carrito.routes.js";
 import 'dotenv/config';
 import cors from 'cors';
+import path from "path";
 
 const app = express()
 app.use(cors());
 app.use(express.json());
-app.use("/imagenes", express.static(path.join("public", "ImgCategorias")));
+app.use("/imagenes", express.static(path.join(process.cwd(), "public", "ImgCategorias")));
 
 app.use('/api/v1', usuariosR);
 app.use('/api/v1', productosR);
