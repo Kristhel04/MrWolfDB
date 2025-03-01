17c2e27be81d6fb85e5ef294d0d899627d84b133
Kimyu15 <kimberley.gomez.quesada@est.una.ac.cr>
2025-03-01 09:10:00 -0600

    .

--- Bakend/src/Controller/ProductoController.js
+++ Bakend/src/Controller/ProductoController.js
@@ -141,21 +141,27 @@ const ProductoController = {
                 where: { genero_dirigido: "Masculino" },
                 include: [{ model: Imagen, as: "imagenes" }]
             });
+            if (productos.length === 0) {
+                return res.status(404).json({ message: "No hay productos masculinos disponibles" });
+            }
             res.status(200).json(productos);
         } catch (error) {
             res.status(500).json({ message: "Error al obtener los productos masculinos", error });
         }
     },
 
-    async ProductosMasculinos(req, res) {
+    async ProductosFemeninos(req, res) {
         try {
             const productos = await Producto.findAll({
                 where: { genero_dirigido: "Femenido" },
                 include: [{ model: Imagen, as: "imagenes" }]
             });
+            if (productos.length === 0) {
+                return res.status(404).json({ message: "No hay productos femeninos disponibles" });
+            }
             res.status(200).json(productos);
         } catch (error) {
-            res.status(500).json({ message: "Error al obtener los productos masculinos", error });
+            res.status(500).json({ message: "Error al obtener los productos femeninos", error });
         }
     }
 
--- Bakend/src/router/Producto.routes.js
+++ Bakend/src/router/Producto.routes.js
@@ -9,5 +9,7 @@ router.post('/productos/buscar', ProductoController.busqueda);
 router.post("/productos", upload.array("imagenes", 5), ProductoController.create);
 router.put("/productos/:id", upload.array("imagenes", 5), ProductoController.update);
 router.delete('/productos/:id', ProductoController.delete);
+router.get('/productos/M',ProductoController.ProductosMasculinos);
+router.get('/productos/F',ProductoController.ProductosFemeninos);
 
 export default router;
