04fa6534cf85e73f7d1b12cf004b66ae2c678679
Kimyu15 <kimberley.gomez.quesada@est.una.ac.cr>
2025-03-07 00:56:20 -0600

    .

--- Bakend/src/index.js
+++ Bakend/src/index.js
@@ -1,6 +1,11 @@
-import app from './app.js';
+import app from './app.js'
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+  
