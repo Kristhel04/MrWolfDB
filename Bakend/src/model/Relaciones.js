a37edff2de1fc6412c12d0dd57482e0c438a8b42
Kimyu15 <kimberley.gomez.quesada@est.una.ac.cr>
2025-03-29 10:03:22 -0600

    .

--- Bakend/src/model/Relaciones.js
+++ Bakend/src/model/Relaciones.js
@@ -2,19 +2,27 @@ import Producto from './ProductoModel.js';
 import Categoria from './CategoriaModel.js';
 import Imagen from './ImagenModel.js';
 import Talla from './TallaModel.js';
-import ProductoTalla from'./ProductoTallaModel.js';
+import ProductoTalla from './ProductoTallaModel.js';
+import Carrito from './CarritoModel.js';
+import CarritoProducto from './CarritoProductoModel.js';
 
+// Relación Categoría - Producto (1:N)
 Categoria.hasMany(Producto, { foreignKey: 'id_categoria', as: 'productos' });
 Producto.belongsTo(Categoria, { foreignKey: 'id_categoria', as: 'categoria' });
 
-Producto.hasMany(Imagen, { foreignKey: "id_producto", as:'imagenes'});
-Imagen.belongsTo(Producto, { foreignKey: "id_producto",as:'producto' });
+// Relación Producto - Imagen (1:N)
+Producto.hasMany(Imagen, { foreignKey: "id_producto", as: 'imagenes' });
+Imagen.belongsTo(Producto, { foreignKey: "id_producto", as: 'producto' });
 
+// Relación Producto - Talla (N:M)
 Producto.belongsToMany(Talla, { through: ProductoTalla, foreignKey: 'id_producto', as: 'tallas' });
-Talla.belongsToMany(Producto, { through: ProductoTalla, foreignKey: 'id_talla', as: 'product' });
+Talla.belongsToMany(Producto, { through: ProductoTalla, foreignKey: 'id_talla', as: 'productos' });
 
+// Relación Carrito - Producto (N:M a través de CarritoProducto)
+Carrito.belongsToMany(Producto, { through: CarritoProducto, foreignKey: 'id_carrito', as: 'productos' });
+Producto.belongsToMany(Carrito, { through: CarritoProducto, foreignKey: 'id_producto', as: 'carritos' });
 
-
+// Mensaje de confirmación
 export default function ConfRelaciones() {
     console.log("Asociaciones configuradas correctamente");
 }
