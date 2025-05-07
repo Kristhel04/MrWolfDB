75976615d88c8bc5f38641853624708b318f7ba4
Kimyu15 <kimberley.gomez.quesada@est.una.ac.cr>
2025-05-06 18:18:58 -0600

    .

--- Bakend/src/router/Factura.route.js
+++ Bakend/src/router/Factura.route.js
@@ -7,7 +7,7 @@ router.get('/factura/:id', authenticateToken, FacturaController.verFacturaPorId)
 router.post('/crear/Factura', authenticateToken, FacturaController.create);
 router.get('/facturas', authenticateToken, FacturaController.getFacturasUsuario);
 router.get('/admin', authenticateToken, authorizeRole(['Administrador']), FacturaController.getAll);
-router.delete('/delete/factura',authenticateToken, authorizeRole(['Administrador']),FacturaController.eliminarFactura)
+router.delete('/delete/:id',authenticateToken, authorizeRole(['Administrador']),FacturaController.eliminarFactura)
 // Descargar factura en PDF
 router.get('/pdf/:id',FacturaController.generarFacturaPDF);
 
