392d369ea7553435cd733c7a1a5c17c5f0a0d6c1
Jertroll <jeremyblanco20@gmail.com>
2025-06-18 01:08:21 -0600

    Update sessionConfig.js

--- src/Middleware/sessionConfig.js
+++ src/Middleware/sessionConfig.js
@@ -9,7 +9,7 @@ const sessionMiddleware = session({
     cookie: {
         httpOnly: true,
         secure: true, // Asegúrate de que esté en false en local, pero en producción debe ser true
-        sameSite: "lax", // Cambia a 'none' si usas HTTPS en producción
+        sameSite: 'none', // Cambia a 'none' si usas HTTPS en producción
         maxAge: 24 * 60 * 60 * 1000 // 1 día
     } // 1 día de duración
 });
