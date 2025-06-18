0fe86c482d12617e380e145a7f7af003c0e5a1cc
Jertroll <jeremyblanco20@gmail.com>
2025-06-17 23:05:07 -0600

    Update sessionConfig.js

--- src/Middleware/sessionConfig.js
+++ src/Middleware/sessionConfig.js
@@ -1,25 +1,17 @@
 import session from "express-session";
-import RedisStore from "connect-redis";
-import Redis from "ioredis";
+console.log("Middleware de sesión cargado correctamente");
 
-const redisClient = new Redis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
-  tls: {} // Necesario si usás Redis en Azure con SSL
-});
 
 const sessionMiddleware = session({
-  store: new RedisStore({ client: redisClient }),
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    httpOnly: true,
-    secure: true,     // ✅ true en producción con HTTPS
-    sameSite: "none", // ✅ para trabajar con 
-    maxAge: 24 * 60 * 60 * 1000
-  }
+    secret: process.env.SESSION_SECRET, 
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        secure: true,           // ✅ importante: true si usas HTTPS
+        sameSite: "none",       // ✅ permite compartir cookies entre dominios
+        maxAge: 24 * 60 * 60 * 1000 // 1 día
+    }
 });
 
 export default sessionMiddleware;
